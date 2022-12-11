6ac809433962a5a6db947ed0fcb4fc607a742094
PochimireddyHarika <118982363+PochimireddyHarika@users.noreply.github.com>
2022-12-11 20:09:55 +0530

    Add files via upload

---
+++ Class-58-update dictionary-CipherSchools.py
@@ -0,0 +1,11 @@
+user_info = {
+    'name' : 'harika',
+    'age' : 18,
+    'favorite movies' : ['coco', 'kimi no na wa'],
+
+}
+more_info = {'name': 'harika pochimireddy', 'state': 'AP'}
+
+user_info.update({})
+print(user_info)
+
---
+++ Class-9-Summary-CipherSchools.py
@@ -0,0 +1,27 @@
+# if statement
+# name = "pochimireddy harika":
+# if name == "pochimireddy harika":
+#    print("you are pochimireddy harika")
+# elif name == "sai":
+#    print("you are sai")
+# else:
+# print("you are not pochimireddy harika or sai")
+
+# while
+# i = 0
+# while i < 10:
+#   print(i)
+#   i += 1
+# for Loop
+# for i in range(1,11):
+#   if i==5:
+#      break
+#   print(i)
+
+# continue keyword
+# for i in range(1,11):
+#    if i == 5:
+#      continue
+#    print(i)
+
+
